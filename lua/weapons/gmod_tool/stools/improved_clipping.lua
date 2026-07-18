6ded8d6ae7096b9e980946c5e13755aea6d3f184
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-07-17 22:22:20 -0400

    Add more options to menu

--- lua/weapons/gmod_tool/stools/improved_clipping.lua
+++ lua/weapons/gmod_tool/stools/improved_clipping.lua
@@ -1,20 +1,59 @@
+-- https://wiki.facepunch.com/gmod/Structures/TOOL
+-- https://wiki.facepunch.com/gmod/Tool_Information_Display
 TOOL.Category = "Construction"
 TOOL.Name = "#tool.improved_clipping.name"
 TOOL.Command = nil
 TOOL.ConfigName = ""
 TOOL.Information = {
-	{ name = "left0", stage = 0 },
+	{ name = "left0", stage = 0, op = 0 },
+	{ name = "left1", stage = 0, op = 1 },
 	{ name = "right0", stage = 0 },
 	{ name = "reload0", stage = 0 },
 }
+
+local ConVarDefaults = {
+	["max_visual_client"] = "8",
+	["physical_clip"]     = "1",
+	["keep_mass"]         = "1",
+	["seal_holes"]        = "1",
+	["add_undo"]          = "1",
+	["mode"]              = "0",
+	["offset"]            = "0",
+}
+
+for Name, Default in pairs(ConVarDefaults) do TOOL.ClientConVar[Name] = Default end
 
 if CLIENT then
 	language.Add("tool.improved_clipping.name", "Improved Clipping")
 	language.Add("tool.improved_clipping.desc", "Applies visual/physical clips to props, changing their visuals/geometry")
 
-	language.Add("tool.improved_clipping.left0", "Define clipping plane(s)")
+	language.Add("tool.improved_clipping.left0", "Define clipping planes. Last 2 planes used.")
+	language.Add("tool.improved_clipping.left1", "Define clipping plane")
+
 	language.Add("tool.improved_clipping.right0", "Add clip to selected entity")
 	language.Add("tool.improved_clipping.reload0", "Clear all clips on selected entity")
+
+	function TOOL.BuildCPanel(Panel)
+		Panel:NumSlider("Max Visual Clips", "improved_clipping_max_visual_client", 1, 8, 0)
+
+		Panel:CheckBox("Physical clip", "improved_clipping_physical_clip")
+		Panel:CheckBox("Keep mass when physics clipping", "improved_clipping_keep_mass")
+		Panel:CheckBox("Seal holes", "improved_clipping_seal_holes")
+		Panel:CheckBox("Add clips to undo list", "improved_clipping_add_undo")
+
+		local Mode = Panel:ComboBox("Mode", "improved_clipping_mode")
+		Mode:AddChoice("2-Hitplanes Intersection", 0)
+		Mode:AddChoice("1-Hitplane", 1)
+
+		Panel:NumSlider("Offset", "improved_clipping_offset", -10, 10, 2)
+
+		local ResetButton = Panel:Button("Reset Values")
+		function ResetButton:DoClick()
+			for Name, Default in pairs(ConVarDefaults) do
+				RunConsoleCommand("improved_clipping_" .. Name, Default)
+			end
+		end
+	end
 
 	local OverlayColor = Color(0, 255, 0)
 	local OverlayMaterial = Material("models/debug/debugwhite")
@@ -76,5 +115,7 @@ function TOOL:Reload(Trace)
 end
 
 function TOOL:Think()
+	self:SetOperation(self:GetClientNumber("mode", 0))
+
 	if CLIENT then return true end
 end
